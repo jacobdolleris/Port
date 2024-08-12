ea8c62d11ba64732ef69fc5c13eaa18040d6457a
Jacob Dolleris <J.dolleris@hotmail.com>
2024-08-12 14:04:15 +0200

    test1

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+
+
+</script>
+
+<template>
+  
+  <header>
+  
+    <ul class="snip1226">
+  <li class="nav-btn current"><a href="#" data-hover="Home">Home</a></li>
+  <!--<li class="nav-btn"><a href="#about" data-hover="About">About</a></li>-->
+  <li class="nav-btn"><a href="#work" data-hover="Work">Work</a></li>
+  <li class="nav-btn"><a href="#education" data-hover="Education">Education</a></li>
+  <li class="nav-btn"><a href="#contact" data-hover="Contact">Contact</a></li>
+  <li><h2>(Work in progress)</h2></li>
+  
+</ul>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+
+
+
+</style>
+<script>
+  document.addEventListener("DOMContentLoaded", function() {
+    var btns = document.querySelectorAll(".nav-btn");
+
+    btns.forEach(function(btn) {
+      btn.addEventListener("click", function() {
+        var currentActive = document.querySelector(".nav-btn.current");
+        if (currentActive) {
+          currentActive.classList.remove("current");
+        }
+        this.classList.add("current");
+      });
+    });
+  });
+</script>
+
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,161 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+
+
+
+  <!--    Home Section    -->  
+<section class="sec section_one" id="home">
+
+  <h1>Home</h1>
+
+  <div class="content">
+<h2>Jacob Dolleris</h2> <br>
+<p>I am a web developer with a deep passion for crafting visually appealing and highly functional websites. My journey in web development has equipped me with extensive knowledge of various programming languages such as HTML, CSS, and JavaScript, as well as modern frameworks like Vue.js. I specialize in creating intuitive website layouts, writing clean and efficient code, and rigorously testing website functionality to ensure that it meets the specific needs and expectations of my clients or organization.  In addition to my technical skills, I have a background in multimedia design, which allows me to approach projects with a keen eye for aesthetics and user experience. This combination of technical expertise and creative insight ensures that the websites I develop are not only robust and reliable but also engaging and user-friendly.  I am highly motivated by a desire to continually expand my knowledge and stay at the forefront of industry trends. I am committed to learning new technologies and techniques that can enhance my work and contribute to the success of the projects I undertake.  Beyond my professional skills, I am a social person who values collaboration and communication. I believe that the best results come from working closely with clients, team members, and stakeholders to fully understand their goals and vision. This approach allows me to deliver solutions that are tailored to the specific needs of each project while fostering strong, positive relationships with those I work with.  Overall, my goal is to create websites that not only meet but exceed expectations, combining functionality, design, and innovation to deliver exceptional online experiences. Whether working on a small personal project or a large-scale corporate website, I bring the same level of dedication, creativity, and professionalism to every task.</p>   
+  
+  </div>
+</section>
+
+<!--
+    About Section     
+<section class="sec section_two" id="about">
+  <h1>About</h1>
+
+  <div class="content">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio quod dolores modi provident porro consectetur odit adipisci natus, soluta excepturi ex maxime, quo voluptates iusto cum, accusantium eaque harum aliquam voluptate sint sapiente. Eveniet, magnam animi dolorum ducimus nisi, amet ipsum cupiditate, quo unde nihil explicabo non maiores tenetur qui.</p>
+    <img src="../components/placeholder.jpeg" alt="">
+  </div>
+
+  <div class="content">
+    <img src="../components/placeholder.jpeg" alt="">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio quod dolores modi provident porro consectetur odit adipisci natus, soluta excepturi ex maxime, quo voluptates iusto cum, accusantium eaque harum aliquam voluptate sint sapiente. Eveniet, magnam animi dolorum ducimus nisi, amet ipsum cupiditate, quo unde nihil explicabo non maiores tenetur qui.</p>
+  </div>
+</section>
+-->
+
+
+<!--    Work Section    --> 
+<section class="sec section_three" id="work">
+
+  <h1>Work</h1>
+
+  <div class="content">
+    <div>
+    <h3>Webdock</h3>
+    <p>My work on developing Webdocks' new landing pages and building the theme for the dashboard post-login, I utilized my deep expertise in PHP, JavaScrip, HTML, and CSS. I created dynamic and responsive user interfaces that provided a seamless and enjoyable experience for our users. In collaboration with external UI & UX designers, I ensured that our platform was not only aesthetically pleasing but also intuitive and easy to navigate. This collaboration helped ensure that the design of our landing pages and dashboard aligned with the latest trends and best practices in user interface design.  As part of my bachelor project, I was responsible for designing and implementing a new purchasing flow that allowed customers to configure their desired servers efficiently. This required not only technical skills but also a thorough understanding of user needs and purchasing behavior. Through this process, I was able to demonstrate my abilities in project management, problem identification and resolution, and communication with stakeholders across various fields.</p>
+  </div>
+
+    <img src="../components/webdock.png" alt="">
+  </div>
+
+  <div class="content">
+    <img src="../components/danpanel.png" alt="">
+    <div> 
+    <h3>Danpanel</h3>
+    <p>Throughout the development of Danpanel's new website in WordPress, my skills in WordPress development saw significant growth. I was responsible for the development of their new website in WordPress. In addition to the development itself, I also had the freedom to choose styling, UX (user experience), UI (user interface), and create a style guide. This allowed me to create a website that not only met their technical requirements but also reflected their brand identity and delivered a high-quality user experience. I took into account both aesthetics and functionality to ensure that the website was easy to navigate, visually appealing, and met the users' needs. As I navigated the development of their new website, I encountered various challenges and opportunities that allowed me to expand my proficiency in WordPress.</p>
+  </div> 
+  </div>
+    <div class="content">
+      <div>
+        <h3>RTID By LUDO</h3>
+        <p>Being involved in the development of both the European and Asian divisions at RTID by Ludo® offers me a unique opportunity to explore and understand distinct market preferences and design aesthetics across different parts of the world. This hands-on experience allows me to delve into market research, identify emerging trends, and refine our product offerings accordingly. Additionally, I play a key role in streamlining processes such as inventory management to ensure smooth operations and optimal stock levels. By focusing on development aspects, I contribute to enhancing our presence and competitiveness in diverse global markets.</p>    
+    
+    </div>
+    <img src="../components/rtidbyludo.jpeg" alt="">
+  </div>
+</section>
+
+
+<!--    Education Section    --> 
+<section class="sec section_four" id="education">
+
+<h1>Education</h1>
+
+<div class="content">
+  <div>
+    <h3>Web developer - University College Lillebælt</h3>
+    <p>At University College Lillebælt, I focused on frontend tools, User Experience (UX), and modern frontend development. I gained practical experience in setting up web applications using various frameworks and GIT for version control. Additionally, I explored IT security as an elective, where I was introduced to Python among other concepts.</p>  
+  </div>
+<img src="../components/ucl.jpeg" alt="">
+</div>
+
+<div class="content">
+  <div>
+    <h3>Multimedia Design - Copenhagen Business Academy</h3>
+    <p>Education in UX and UI, including content creation and enhancing coding skills. Many tasks in this program involved advertising and communication with potential clients. As an elective, I chose Immersive Playground, providing access to projects in Unity and an introduction to C#.</p>  
+  </div>
+<img src="../components/copenhagen-b-a.png" alt="">
+</div>
+
+<div class="content">
+  <div>
+    <h3>HTX Kommunikation & IT - Ehvervskolen Nordsjælland</h3>
+    <p>Education in UX and UI with an introduction to coding. Introduction to various IT tools was also part of the curriculum. The program focused on establishing user connections through IT solutions. Most projects during the course were developed using prototype software.</p>  
+  </div>
+<img src="../components/esnord.jpeg" alt="">
+</div>
+</section>
+
+
+
+<!-- About Section -->    
+<section class="sec section_two" id="contact">
+  <h1>Contact</h1>
+
+  <div class="background">
+  <div class="container">
+    <div class="screen">
+      <div class="screen-header">
+        <div class="screen-header-left">
+          <div class="screen-header-button close"></div>
+          <div class="screen-header-button maximize"></div>
+          <div class="screen-header-button minimize"></div>
+        </div>
+        <div class="screen-header-right">
+          <div class="screen-header-ellipsis"></div>
+          <div class="screen-header-ellipsis"></div>
+          <div class="screen-header-ellipsis"></div>
+        </div>
+      </div>
+      <div class="screen-body">
+        <div class="screen-body-item left">
+          <div class="app-title">
+            <span>Jacob</span>
+            <span>Dolleris</span>
+          </div>
+        </div>
+        <div class="screen-body-item">
+          <div class="app-form">
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="NAME">
+            </div>
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="EMAIL">
+            </div>
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="CONTACT NO">
+            </div>
+            <div class="app-form-group message">
+              <input class="app-form-control" placeholder="MESSAGE">
+            </div>
+            <div class="app-form-group buttons">
+              <button class="app-form-button" style="margin-right: 30px;">CANCEL</button>
+              <button class="app-form-button">SEND</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+  </div>
+</div>
+
+</section>
+
+
+
+
+  </main>
+</template>
